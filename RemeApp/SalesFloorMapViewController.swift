3ea2d60a1a7b88e128552ba806f702d2aa923fe6
HaruhikoMotokawa <123522778+HaruhikoMotokawa@users.noreply.github.com>
2023-03-21 00:03:59 +0900

    main.Storyboardの削除、StoryboardReferenceの設定

---
+++ RemeApp/SalesFloorMapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SalesFloorMapViewController.swift
+//  RemeApp
+//
+//  Created by 本川晴彦 on 2023/03/20.
+//
+
+import UIKit
+
+class SalesFloorMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
