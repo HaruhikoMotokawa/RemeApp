c2af8734d128d51c3f7f35ecbfd48a86db631cbe
HaruhikoMotokawa <123522778+HaruhikoMotokawa@users.noreply.github.com>
2023-03-22 00:42:58 +0900

    売り場マップ閲覧画面のUIButton、UILabelの設置とIBOutletまで完了

--- RemeApp/SalesFloorMapViewController.swift
+++ RemeApp/SalesFloorMapViewController.swift
@@ -9,21 +9,86 @@ import UIKit
 
 class SalesFloorMapViewController: UIViewController {
 
+
+    @IBOutlet weak var aOneButton: UIButton!
+
+
+    @IBOutlet weak var aTwoButton: UIButton!
+
+
+    @IBOutlet weak var aThreeButton: UIButton!
+
+
+    @IBOutlet weak var bOneButton: UIButton!
+
+
+
+    @IBOutlet weak var bTwoButton: UIButton!
+
+
+    @IBOutlet weak var bThreeButton: UIButton!
+
+
+    @IBOutlet weak var bFourButton: UIButton!
+
+
+    @IBOutlet weak var bFiveButton: UIButton!
+
+
+    @IBOutlet weak var bSixButton: UIButton!
+
+
+    @IBOutlet weak var bSevenButton: UIButton!
+
+
+    @IBOutlet weak var cOneButton: UIButton!
+
+
+    @IBOutlet weak var cTwoButton: UIButton!
+
+
+    @IBOutlet weak var cThreeButton: UIButton!
+
+
+    @IBOutlet weak var cFourButton: UIButton!
+
+
+    @IBOutlet weak var cFiveButton: UIButton!
+
+
+    @IBOutlet weak var cSixButton: UIButton!
+
+
+    @IBOutlet weak var cSevenButton: UIButton!
+
+
+    @IBOutlet weak var registerLabel: UILabel!
+
+    @IBOutlet weak var leftEntranceLabel: UILabel!
+
+    @IBOutlet weak var rightEntranceLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 各UILabelに枠線を設定
+        setBorderForLabel(label: registerLabel)
+        setBorderForLabel(label: leftEntranceLabel)
+        setBorderForLabel(label: rightEntranceLabel)
 
-        // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // UILabelに枠線を設定するメソッド
+    func setBorderForLabel(label: UILabel) {
+        let borderColor = UIColor.black.cgColor
+        label.layer.borderColor = borderColor
+        label.layer.borderWidth = 2
+        label.layer.cornerRadius = 10
+        label.sizeToFit()
     }
-    */
+
+    // 各UIButtonに名称と色を設定するメソッド
+    //    func setButtonTitle(salesFloorType: SalesFloorType) {
+    //        let salesFloorType = salesFloorType.nameOfSalesFloor
+    //    }
 
 }
