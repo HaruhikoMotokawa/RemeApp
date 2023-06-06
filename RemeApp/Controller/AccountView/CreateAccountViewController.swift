75d6acfe8fd396ecc25655fc8637d94e744bde34
HaruhikoMotokawa <123522778+HaruhikoMotokawa@users.noreply.github.com>
2023-06-06 15:45:19 +0900

    アカウント関連のUI作成開始

---
+++ RemeApp/Controller/AccountView/CreateAccountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateAccountViewController.swift
+//  RemeApp
+//
+//  Created by 本川晴彦 on 2023/06/06.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
