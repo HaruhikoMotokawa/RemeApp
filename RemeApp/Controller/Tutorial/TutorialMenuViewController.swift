d1a9da0472664d57da8788d81454d2d9b8015914
HaruhikoMotokawa <123522778+HaruhikoMotokawa@users.noreply.github.com>
2023-09-26 10:55:56 +0900

    CocoaPods関連アップデート
    1.CocoaPodsを1.13にアップデート
    2.Podfileにターゲットを15.0以上にするよう追記

---
+++ RemeApp/Controller/Tutorial/TutorialMenuViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TutorialMenuViewController.swift
+//  RemeApp
+//
+//  Created by 本川晴彦 on 2023/09/26.
+//
+
+import UIKit
+
+final class TutorialMenuViewController: UIViewController {
+
+
+
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = UIColor.systemBackground
+
+  }
+}
+
+#Preview {
+  let vc = TutorialMenuViewController()
+  return vc
+}
